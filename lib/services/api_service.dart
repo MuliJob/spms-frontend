d477ce088f5af6fa124948dd4360a3b239406d48
MuliJob <munyoki912@gmail.com>
2025-07-09 13:27:21 +0300

    api service for getting supervisor proposal

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -268,4 +268,18 @@ class ApiService {
       throw Exception('Failed to load assigned projects');
     }
   }
+
+  static Future<List<dynamic>> getSupervisorProposals(String token) async {
+    final response = await http.get(
+       Uri.parse('$baseUrl/api/supervisor/proposals/'),
+       headers: {'Authorization': 'Token $token'},
+    );
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    }   else {
+        throw Exception('Failed to load proposals');
+    }
+  }
+
 }
